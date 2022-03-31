0bce091ef8729f763860aca88093a9eec45dca91
arician <arician@live.com>
2022-03-31 11:00:08 +0100

    2p started, home page made

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+// 1p version
+
 let scorecard=document.getElementById("scorecard");
 const roll=document.getElementById("roll");
 const restart=document.getElementById("restart");
 let state=document.getElementById("gamestate");
+const dice=document.getElementById("dice");
 
+let diceImg={
+    1:"images/1.png",
+    2:"images/2.png",
+    3:"images/3.png",
+    4:"images/4.png",
+    5:"images/5.png",
+    6:"images/6.png",
+}
 let score=0
 let thisRoll=0
 const gameover=() => {
@@ -10,7 +21,9 @@ const gameover=() => {
     restart.style.display="block"
 }
 let diceroll=()=>{
-    thisRoll=Math.floor(Math.random()*5+1)
+    thisRoll=Math.floor(Math.random()*5.99+1)
+    dice.src=diceImg[thisRoll]
+    console.log(thisRoll)
 }
 const scoreupdate=() => {scorecard.textContent=score}
 roll.addEventListener("click",() => {
@@ -32,3 +45,8 @@ restart.addEventListener("click",() => {
     restart.style.display="none"
     state.textContent=""
 })
+
+
+// 2p game
+let tptitle=document.getElementById("2pTitle")
+let subTitle=document.getElementById("2pSubTitle")
